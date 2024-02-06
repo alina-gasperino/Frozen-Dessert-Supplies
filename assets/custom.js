df0afc38785adb8434299d458a770ad9e9e29d2c
alina-gasperino <alinacreatesthings@gmail.com>
2024-02-06 00:28:51 -0500

    first product template created

--- assets/custom.js
+++ assets/custom.js
@@ -5,4 +5,8 @@ $(document).ready(function () {
             $(this).closest(".variant-wrapper").find(".var_val").text(value)
         })
     })
+
+    $(".add-to-cart-2").click(function() {
+        $(".product-single__form .btn.btn--full.add-to-cart").click()
+    })
 });
